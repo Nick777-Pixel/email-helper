22c6666b7f06ad2776b7eaf3d0f5b91233dc004a
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-01-30 23:35:53 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -100,16 +100,6 @@ const Home: NextPage = () => {
 
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-            href="https://github.com/shengxinjing/email-helper"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github />
-            <p>Star on GitHub</p>
-          </a>
-
-          <a
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
             href="https://shengxinjing.cn/react/"
             target="_blank"
             rel="noopener noreferrer"
